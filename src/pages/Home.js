5ebde46b40549f6e02c96126f3722a50d1bb96a7
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-08-07 22:15:10 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import Banner from '../components/Banner'
-import Search  from '../components/Search'
-import HouseList from '../components/HouseList'
-import Footer from '../components/Footer'
+import Banner from '../components/Banner';
+import Search from '../components/Search';
+import HouseList from '../components/HouseList';
+import Footer from '../components/Footer';
+import { useTheme } from '../ThemeContext'; // Import the useTheme hook
 
 const Home = () => {
-  return (
-       <div className='min-h-[1800px]'>
-        <Banner/>
-        <Search/>
-        <HouseList/>
-        <Footer />
-       </div>
+  const { isNightMode } = useTheme(); // Use the isNightMode value from the theme context
 
+  const pageStyles = {
+    backgroundColor: isNightMode ? '#141414' : '#ffffff', // Adjust colors as needed
+    color: isNightMode ? '#ffffff' : '#000000',
+    minHeight: '1800px',
+  };
+
+  return (
+    <div style={pageStyles}>
+      <Banner />
+      <Search />
+      <HouseList />
+      <Footer />
+    </div>
   );
 };
 
